a09d30102b759d8f4eb10f86e90905e92c0d74ee
Aleksey <alekseykarpelev@gmail.com>
2022-07-04 21:44:00 +1000

    fix  migration pages

--- database/migrations/2022_07_02_231317_create_pages_table.php
+++ database/migrations/2022_07_02_231317_create_pages_table.php
@@ -15,9 +15,9 @@ class CreatePagesTable extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('link');
-            $table->json('block');
+            $table->string('name')->nullable();
+            $table->string('link')->nullable();
+            $table->json('block')->nullable();
             $table->timestamps();
         });
     }
