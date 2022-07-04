1af9629ff3d2a4bef4ba4e2da1e4a2d59b077390
Aleksey <alekseykarpelev@gmail.com>
2022-07-04 21:28:54 +1000

    add   demo

---
+++ app/Providers/ContentServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+      public  $content ;
+
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
